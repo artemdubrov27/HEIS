8e9785869add7bc14c79d6915e309459a31ffa3e
artemdubrov27 <art.dubrov27@gmail.com>
2026-08-12 14:23:22 +0300

    Fix SQLite path for Render

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "sqlite:///./data/heis.db"
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, '../data/heis.db')}"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
